30daf47feb8b52ce7fb09afff006215778321ffe
QDDse <XJTU3121358312@163.com>
2022-10-14 06:17:24 +0000

    update 3

--- models/swin_transformer_moe.py
+++ models/swin_transformer_moe.py
@@ -731,6 +731,7 @@ class SwinTransformerMoE(nn.Module):
         self.init_std = init_std
         self.aux_loss_weight = aux_loss_weight
         self.num_local_experts = num_local_experts
+        ## 每层layer的expert—num
         self.global_experts = num_local_experts * dist.get_world_size() if num_local_experts > 0 \
             else dist.get_world_size() // (-num_local_experts)
         self.sharded_count = (1.0 / num_local_experts) if num_local_experts > 0 else (-num_local_experts)
