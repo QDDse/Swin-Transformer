ad34b48407b2ca9b7811dc5d09e0a584050ed4af
QDDse <XJTU3121358312@163.com>
2022-11-23 08:16:11 +0000

    add shell

--- requirements.sh
+++ requirements.sh
@@ -2,12 +2,14 @@
 git clone https://github.com/facebookresearch/fairscale.git
 cd fairscale 
 git tag 0.4.6
-
+git am --abort
+git am --signoff < ../patch/*.patch
 pip install timm
 
 pip install yacs
 pip install pytest
 pip install termcolor
+cd ..
 git+https://github.com/microsoft/tutel@main
 python3 -m pip uninstall tutel -y
 python3 ./tutel/setup.py install --user
